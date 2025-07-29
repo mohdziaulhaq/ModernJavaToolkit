8a88045deb47da6642750a0f6042dc9efd52214f
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-29 05:05:54 -0500

    Consumer accept()

---
+++ src/functionalinterfaces/ConsumerExample.java
@@ -0,0 +1,11 @@
+package functionalinterfaces;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        Consumer<String> consumer = (s) -> System.out.println(s.toUpperCase());
+
+        consumer.accept("Hello");
+    }
+}
