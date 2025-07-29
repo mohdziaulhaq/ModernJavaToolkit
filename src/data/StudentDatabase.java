20ecd4c185b78ba5b013bd64dd3e193ecad4c8a9
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-29 05:31:35 -0500

    StudentDatabase.java

--- src/data/StudentDatabase.java
+++ src/data/StudentDatabase.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class StudentDatabase {
     public static List<Student> getStudents() {
-        Student student = new Student("Adam",2,3.6,"male", Arrays.asList("swimming","basketball","soccer"));
 
         Student student1 = new Student("Adam", 2, 3.6, "male", Arrays.asList("swimming", "basketball", "soccer"));
         Student student2 = new Student("Emily", 3, 3.9, "female", Arrays.asList("reading", "chess", "volleyball"));
@@ -15,7 +14,7 @@ public class StudentDatabase {
         Student student6 = new Student("Olivia", 3, 3.7, "female", Arrays.asList("writing", "music", "yoga"));
         Student student7 = new Student("Ethan", 2, 2.5, "male", Arrays.asList("skateboarding", "math club"));
         Student student8 = new Student("Ava", 4, 4.0, "female", Arrays.asList("robotics", "debate", "reading"));
-        List<Student> students = Arrays.asList(student,student1,student2,student3,student4,student5,student6,student7,student8);
+        List<Student> students = Arrays.asList(student1,student2,student3,student4,student5,student6,student7,student8);
         return students;
 
     }
